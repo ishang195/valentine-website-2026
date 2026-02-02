abc58e472ac09df8aa626a7cc2fb2088be3a4cf3
ishang195 <ishang195@gmail.com>
2026-02-02 11:25:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -61,8 +61,8 @@ const CONFIG = {
     colors: {
         backgroundStart: "#FCC6FF",      // Gradient start (try pastel colors for a soft look)
         backgroundEnd: "#FFA09B",        // Gradient end (should complement backgroundStart)
-        buttonBackground: "#ff6b6b",     // Button color (should stand out against the background)
-        buttonHover: "#ff8787",          // Button hover color (slightly lighter than buttonBackground)
+        buttonBackground: "#FFC785",     // Button color (should stand out against the background)
+        buttonHover: "#FFE6C9",          // Button hover color (slightly lighter than buttonBackground)
         textColor: "#ff4757"             // Text color (make sure it's readable!)
     },
 

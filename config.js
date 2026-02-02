e680aac24bd3ed739d181a91b5b6886431b41583
ishang195 <ishang195@gmail.com>
2026-02-02 11:31:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💜', '💘'],  // Heart emojis
-        bears: ['🧸', '🐻'],                       // Cute bear emojis
-        misc: ['💐','💋']
+        bears: ['🧸', '🐻','💐','💋']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -43,10 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "Whoa You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰",                           // Shows when they go past 100%
-        low: "That's it? 🥹"
     },
 
     // Messages that appear after they say "Yes!"
